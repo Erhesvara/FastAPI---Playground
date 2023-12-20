f2679a2db7d84e66360fc648e8e7933e1caf9241
Erhesvara <106751010+Mahesvaraaaa@users.noreply.github.com>
2023-12-20 23:23:36 +0800

    with "none" value where it has error, I change it to "..."

--- myapi.py
+++ myapi.py
@@ -15,5 +15,5 @@ def index():
     return {"name": "First Data"}
 
 @app.get("/get-student/{student_id}")
-def get_student(student_id: int = Path(None, description="The ID of the student you want to view")):
+def get_student(student_id: int = Path(..., description="The ID of the student you want to view")):
     return students[student_id]
