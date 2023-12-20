16db67159cf1420533195c7271cd614869d9579a
Erhesvara <106751010+Mahesvaraaaa@users.noreply.github.com>
2023-12-20 23:03:59 +0800

    get

--- myapi.py
+++ myapi.py
@@ -2,6 +2,14 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+students = {
+    1: {
+        "name": "John",
+        "age": 17,
+        "class": "year 12"
+    }
+}
+
 @app.get("/")
 def index():
     return {"name": "First Data"}
