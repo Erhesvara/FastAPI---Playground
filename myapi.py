8ef68c4f11f1e1a988d6214e2042bdd7ebd3db89
Erhesvara <106751010+Mahesvaraaaa@users.noreply.github.com>
2023-12-21 00:20:25 +0800

    error: query is not displaying on my docs

--- myapi.py
+++ myapi.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Path, Query
 from typing import Optional
+from pydantic import BaseModel
 
 app = FastAPI()
 
@@ -7,9 +8,14 @@ students = {
     1: {
         "name": "John",
         "age": 17,
-        "class": "year 12"
+        "year": "year 12"
     }
 }
+
+class Student(BaseModel):
+    name: str
+    age: int
+    year: str
 
 @app.get("/")
 def index():
@@ -19,9 +25,17 @@ def index():
 def get_student(student_id: int = Path(..., description="The ID of the student you want to view", gt=0, )):
     return students[student_id]
 
-@app.get("/get-by-name")
-def get_student(*, name: Optional[str] = Query(..., description="Name of the student to search for")):
+@app.get("/get-by-name/{student_id}")
+def get_student(*, student_id: int, name: Optional[str] = Query(..., description="Name of the student to search for")):
     for student_id in students:
         if students[student_id]["name"] == name:
             return students[student_id]
     return {"Data": "Not found"}
+
+@app.post("/create-student/{student_id}")
+def create_student(student_id: int, student : Student):
+    if student_id in students:
+        return {"Error": "Student Already Exists"}
+
+    students[student_id] = student
+    return students[student_id]
